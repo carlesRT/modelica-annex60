2d926d8c1113dcbbc55d8cd93a9322829443b915
Carles <c.ribastugores@aee.at>
2026-06-02 09:53:52 +0200

    remove use of ModelicaPropRef.

--- IBPSA/Resources/src/fluid/heatpumps/calibration/Examples/example_calibration.py
+++ IBPSA/Resources/src/fluid/heatpumps/calibration/Examples/example_calibration.py
@@ -77,18 +77,10 @@ def main():
     eva = hp.heatexchangers.EvaporatorCondenser([0.])
     
     # Refrigerant model
-    # Use Modelica ref (via Dymola)
-    map_libraries = {"IBPSA":r"S:/Carles/Repositories/Carles-IBPSA/IBPSA",
-                     "ThermofluidStream":r"S:/Carles/Repositories/DLR/ThermofluidStream"}  
-    generate_refmaps = True
-    if generate_refmaps:      
-        hp.ref_maps.ModelicaPropRefrigerantMap('R1234yf_ph', 'ThermofluidStream.Media.XRGMedia.R1234yf_ph', map_libraries)   
-
-    ref = hp.refrigerants.ModelicaPropRefrigerant('R1234yf_ph', 'ThermofluidStream.Media.XRGMedia.R1234yf_ph', map_libraries)
     # Use CoolProp
     #ref = hp.refrigerants.CoolPropRefrigerant('R410A', 'IBPSA.Media.Refrigerants.R410ACoolProp')
     # Use Python implemented ref
-    #ref = hp.refrigerants.R410A()
+    ref = hp.refrigerants.R410A()
     
     # Fluid model on condenser side
     fluCon = hp.fluids.ConstantPropertyWater()
